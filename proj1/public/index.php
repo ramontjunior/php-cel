663f4cac641fa59098d3a8e25d53a3b2c992a30c
ramontjunior <ramontjunior@hotmail.com>
2020-08-24 18:45:55 +0000

    Aula 38

--- proj1/public/index.php
+++ proj1/public/index.php
@@ -2,15 +2,22 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Ramont\Controller\PageController;
+
 $url = substr($_SERVER['REQUEST_URI'],1);
 $url = explode('/', $url);
 
 $controller = isset($url[0]) && $url[0] ? $url[0] : 'page';
 $action = isset($url[1]) && $url[1] ? $url[1] : 'index';
 
-$controller = ucfirst($controller) . 'Controller';
+$controller = "Ramont\Controller\\" . ucfirst($controller) . 'Controller';
 
 #print $controller;
 
-$execute = new $controller();
-print $execute->index();
+$response = call_user_func_array([$controller, $action], []);
+
+//$execute = new $controller();
+//$execute = new PageController();
+//print $execute->index();
+
+print $response;
