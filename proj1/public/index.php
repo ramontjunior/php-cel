e5aee0e7c3c1874fbfd683d5e8f9c8823dd542fe
ramontjunior <ramontjunior@hotmail.com>
2020-08-25 18:17:08 +0000

    Correção de erros que não deu pra arrumar ontem - adc $param na linha 20

--- proj1/public/index.php
+++ proj1/public/index.php
@@ -12,16 +12,19 @@ $action = isset($url[1]) && $url[1] ? $url[1] : 'index';
 $param = isset($url[2]) && $url[2] ? $url[2] : null;
 
 
+#Aquui eu verifico se a classe existe
 #$controller = "Ramont\Controller\\" . ucfirst($controller) . 'Controller';
 if(!class_exists($controller = "Ramont\Controller\\" . ucfirst($controller) . 'Controller')){
     die("<h1>404 - Not Found</h1>");
 }
 
+#Aqui eu verifico se o método informado na URL existe dentro da classe
 if(!method_exists($controller, $action)){
     $action = 'index';
     $param  = $url[1];
 }
 
-$response = call_user_func_array([$controller, $action], []);
+#Aqui usa pra dar um "New" sobre a função conforme 
+$response = call_user_func_array([$controller, $action], [$param]);
 
 print $response;
